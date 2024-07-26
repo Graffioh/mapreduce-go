e5a76af3a27615c603d5360f63f2deb8dea946f7
Graffioh <bregliascuola2002@gmail.com>
2024-07-26 20:15:37 +0200

    Implement output file creation

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"wc-mapreduce-go/kv"
 	"wc-mapreduce-go/mr"
 	"wc-mapreduce-go/worker"
@@ -36,4 +38,10 @@ func main() {
 	kva_result := <-reduce_res_chan
 
 	fmt.Printf("\n\nFinal result: %v", kva_result)
+
+	d1 := []byte(fmt.Sprintf("%v\n", kva_result))
+	err := os.WriteFile("./output-file-1.txt", d1, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
